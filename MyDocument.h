8aea9c2fb98876d1c4c1f23087b624c277710c1d
beepscore <support@beepscore.com>
2009-10-17 00:03:47 -0700

    Initial commit.

---
+++ MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  MeetingMeter
+//
+//  Created by Steve Baker on 10/16/09.
+//  Copyright 2009 Beepscore LLC. All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+{
+}
+@end
